219e5857cf6fba808cd1f02edb78bbed84074aac
Veronica Sonzini <mavesonzini@gmail.com>
2017-01-11 15:26:16 +0000

    facebook login

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews
   end
-
-
-
+  
   root "restaurants#index"
 
 end
--- yelp_clone/spec/features/restaurant_feature_spec.rb
+++ yelp_clone/spec/features/restaurant_feature_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add restaurants' do
+      sign_up
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
@@ -10,8 +11,18 @@ feature 'restaurants' do
   end
 end
 
+  context "haven't signed in" do
+    scenario "unable to make changes" do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      expect(page).to have_content "Log in"
+      expect(current_path).to eq('/users/sign_in')
+    end
+  end
+
   context 'restaurants have been added' do
     before do
+      sign_up
       Restaurant.create(name: 'KFC')
     end
     scenario 'display restaurants' do
@@ -23,6 +34,7 @@ end
 
   context 'creating restaurants' do
   scenario 'prompts user to fill out a form, then displays the new restaurant' do
+    sign_up
     visit '/restaurants'
     click_link 'Add a restaurant'
     fill_in 'Name', with: 'KFC'
@@ -33,6 +45,7 @@ end
 
   context 'an invalid restaurant' do
     scenario 'does not let you submit a name that is too short' do
+      sign_up
       visit('/restaurants')
       click_link("Add a restaurant")
       fill_in("Name", with: "KF")
@@ -40,7 +53,7 @@ end
 
       expect(page).not_to have_css("h2", text: "KF")
       expect(page).to have_content("Error")
-    end 
+    end
   end
 end
 
@@ -48,6 +61,7 @@ context 'viewing restaurants' do
   let!(:kfc){ Restaurant.create(name:'KFC') }
 
   scenario 'lets a user view a restaurant' do
+    sign_up
     visit '/restaurants'
     click_link ('KFC')
     expect(page).to have_content('KFC')
@@ -60,6 +74,7 @@ context 'editing restaurants' do
   before{ Restaurant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
 
   scenario 'let a user edit a restaurant' do
+    sign_up
     visit '/restaurants'
     click_link('Edit KFC')
     fill_in('Name', with: 'Kentucky Fried Chicken')
@@ -77,6 +92,7 @@ context 'deleting restaurants' do
   before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
 
   scenario 'removes a restaurant when a user clicks a delete link' do
+    sign_up
     visit '/restaurants'
     click_link('Delete KFC')
     expect(page).not_to have_content('KFC')
