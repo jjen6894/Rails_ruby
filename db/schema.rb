da2a7281b5454e08d30f386a571ba49974cbb840
George Seeger <georgeseeger@hotmail.com>
2017-01-11 10:10:24 +0000

    walkthrough 12 complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110152711) do
+ActiveRecord::Schema.define(version: 20170111100040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,11 @@ ActiveRecord::Schema.define(version: 20170110152711) do
   create_table "reviews", force: :cascade do |t|
     t.text     "thoughts"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -9,7 +9,7 @@ feature 'Reviews' do
       click_link 'Review KFC'
       fill_in 'Thoughts', with: 'so-so'
       select '3', from: 'Rating'
-      click_button 'Leave review'
+      click_button 'Leave Review'
 
       expect(current_path).to eq '/restaurants'
       expect(page).to have_content 'so-so'
