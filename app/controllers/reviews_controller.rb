9025c5a31741027eeeddfbb63b04b976903e2fdf
ejatkin <ejatkin@gmail.com>
2017-01-12 16:35:36 +0000

    Sorted database issues, one test failing two pendng

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,12 +5,6 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
-  def build_review(attributes = {}, user)
-    review = reviews.build(attributes)
-    review.user = user
-    review
-  end
-
   def create
     @restaurant = Restaurant.find params[:restaurant_id]
     @review = @restaurant.build_review review_params, current_user
@@ -19,7 +13,7 @@ class ReviewsController < ApplicationController
       redirect_to restaurants_path
     else
       if @review.errors[:user]
-        redirect_to restaurats_path, alert: 'You have already reviewed this restaurant'
+        redirect_to restaurants_path, alert: 'You have already reviewed this restaurant'
       else
         render :new
       end
