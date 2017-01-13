2ff0bf663fc7d4ecd78a42d87756d15a0b12a5df
ejatkin <ejatkin@gmail.com>
2017-01-13 11:31:22 +0000

    All tests passing

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
   validates :rating, inclusion: (1..5)
-  validates :user, uniqueness:{ scope: :restaurant, message: "has already reviewed this restaurant" }
+  validates :user, uniqueness:{ scope: :restaurant, message: "You have already reviewed this restaurant" }
 end
